90de4de406c903b81242a7f695de0189f64c6eac
Jarrett B <jarrettboice@gmail.com>
2018-10-01 15:15:41 -0700

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# STRP Discord Bot
+
+A Discord Bot that loops through all your servers and displays all of their status via command. It will also add all of the player counts and display it in the Rich Presence!
+
+## Getting Started
+
+1. Clone the repository
+2. Modify the credential and server channel information
+3. ./bot.py
+
+## Acknowledgments
+
+* The STRP FiveM Server for the encouragement and absolute desire to know when the severs would come back up.
--- bot.py
+++ bot.py
@@ -9,9 +9,12 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 client = discord.Client()
 http = urllib3.PoolManager(10,
-                           headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'})
-servers = [["STRP #1", "66.70.180.161:30120", False], ["STRP #2", "66.70.180.161:30121", False], ["STRP #3", "66.70"
-                                        ".180.161:30122",False], ["STRP Whitelist #1", "66.70.180.161:30123", False]]
+headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'})
+# The following is an example. Add as many sevrvers as you would like to the array.
+servers = [["STRP #1", "66.70.180.161:30120", False],
+           ["STRP #2", "66.70.180.161:30121", False],
+           ["STRP #3", "66.70.180.161:30122",False],
+           ["STRP Whitelist #1", "66.70.180.161:30123", False]]
 
 
 def server_online(ip):
